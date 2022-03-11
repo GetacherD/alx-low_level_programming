300f3b622176577e3755a4ba35a755640d633f2e
Getacher Demisse <getacher.d@gmail.com>
2022-03-11 13:14:09 +0300

    combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ * Description: print number comination -unique
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			if ((i != j) && (i < j))
+			{
+				putchar(i);
+				putchar(j);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
