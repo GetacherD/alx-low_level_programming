ffe5083d5028ab9193120dc6987838b66f8213ce
Getacher Demisse <getacher.d@gmail.com>
2022-03-12 12:25:15 +0300

    final

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 			{
 				for (m = 48; m <= 57; m++)
 				{
-					if ((m > j) || (n >= i))
+					if ((n > i) || ((n == i) && (m > j)))
 					{
 						putchar(i);
 						putchar(j);
@@ -27,7 +27,7 @@ int main(void)
 						putchar(n);
 						putchar(m);
 					}
-					if (((m > j) || (n >= i)) &&
+					if (((n > i) || ((n == i) && (m > j))) &&
 						!((i == 57) && (j == 56) && (n == 57) && (m == 57)))
 					{
 						putchar(',');
