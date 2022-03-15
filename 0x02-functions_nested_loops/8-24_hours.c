34a5c4331bce7cf6e7ccf208efa2620d239e34cb
Getacher Demisse <getacher.d@gmail.com>
2022-03-15 21:27:48 +0300

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,20 +12,23 @@ void jack_bauer(void)
 	int n;
 	int m;
 
-	for (i = 48; i <=50; i++)
+	for (i = 48; i <= 50; i++)
 	{
-		for (j = 48; j <=51 ; j++)
+		for (j = 48; j <= 57 ; j++)
 		{
 			for (n = 48; n <= 53; n++)
 			{
 				for (m = 48; m <= 57; m++)
 				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(n);
-					_putchar(m);
-					_putchar('\n');
+					if (((i == 50) && (j <= 51)) || (i <= 49))
+					{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(n);
+						_putchar(m);
+						_putchar('\n');
+					}
 				}
 			}
 		}
