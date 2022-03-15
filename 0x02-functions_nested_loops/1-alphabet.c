3cc23340d7c3e7ce7e6126f72be99144d28e9953
Getacher Demisse <getacher.d@gmail.com>
2022-03-15 15:51:15 +0300

    add space

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,13 @@
 #include "main.h"
-
+/**
+ * print_alphabet -prints alphabet a-z
+ * Description: prints all lower case alp
+ */
 void print_alphabet(void)
 {
 	int i;
-	for (i = 97;i <= 122;i++)
+
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
