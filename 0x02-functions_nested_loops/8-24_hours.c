602286ccd8d6cfb9c7aac46d0dda770b9aae43df
Getacher Demisse <getacher.d@gmail.com>
2022-03-15 20:40:41 +0300

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * jack_bauer -print hours and minutes
+ * Description: print hour and minute
+ *
+ * Return: 0 always
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int n;
+	int m;
+
+	for (i = 48; i <=50; i++)
+	{
+		for (j = 48; j <=51 ; j++)
+		{
+			for (n = 48; n <= 53; n++)
+			{
+				for (m = 48; m <= 57; m++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(n);
+					_putchar(m);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+
+}
