09fd3b2733eeb4849a341cdd31964af51e20ee89
Getacher Demisse <getacher.d@gmail.com>
2022-03-15 18:45:34 +0300

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * int_abs - give abs value
+ * @: any int input
+ *
+ * Description: print abs value
+ *
+ * Return: 0 always
+ */
+int_abs(int x)
+{
+	return abs(x);
+}
+
+
