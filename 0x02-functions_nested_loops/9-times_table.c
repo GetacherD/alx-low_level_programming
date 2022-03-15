f68249900bb57f4a797bf4ad33c0c80cce674856
Getacher Demisse <getacher.d@gmail.com>
2022-03-15 22:24:26 +0300

    test for 9 mult

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,37 @@
 #include "main.h"
 /**
+ * gc -convert asci to num
+ * Description: print char
+ */
+int gc(int c)
+{
+        switch (c)
+        {
+        case 0:
+                return (48);
+        case 1:
+                return (49);
+        case 2:
+                return (50);
+        case 3:
+                return (51);
+        case 4:
+                return (52);
+        case 5:
+                return (53);
+        case 6:
+                return (54);
+        case 7:
+                return (55);
+        case 8:
+                return (56);
+        case 9:
+                return (57);
+        default :
+                return (58);
+        }
+}
+/**
  * times_table -print times table
  * Description: print times table
  */
