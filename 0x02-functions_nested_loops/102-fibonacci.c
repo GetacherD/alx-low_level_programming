2b2990b2cfa9b76de46649aee9a174367ab855fe
Getacher Demisse <getacher.d@gmail.com>
2022-03-16 01:01:56 +0300

    time table

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Description: fibonaci num
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int sum;
+
+	i = 1;
+	j = 2;
+	for (k = 0; k < 50; k++)
+	{
+		sum = i + j;
+		i++;
+		j++;
+		printf("%d, ", sum);
+	}
+	printf("\n");
+
+	return (0);
+}
