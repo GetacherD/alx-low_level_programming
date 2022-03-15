03c9ee4d9668dfd52cc048753ebae2c31c0a4297
Getacher Demisse <getacher.d@gmail.com>
2022-03-15 19:14:40 +0300

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit  -print last digit of an integer
+ * @x: the input number
+ *
+ * Description: print last digit
+ *
+ * Return: last digit
+ */
+int print_last_digit(int x)
+{
+	return (x % 10);
+}
