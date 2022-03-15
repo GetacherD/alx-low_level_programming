94c11c06147dcbf7ba890171a56a217eeeede4ce
Getacher Demisse <getacher.d@gmail.com>
2022-03-16 01:42:46 +0300

    time table

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,30 +7,24 @@
  */
 int main(void)
 {
-	int k;
 	long int sum;
 	long int prev;
 	long int temp;
 	long int sum_even;
 
 	sum_even = 0;
-	k = 0;
 	sum = 2;
 	temp = 1;
 	prev = 1;
-	while (k < 48)
+	while (prev < 400000000)
 	{
 		temp = sum;
 		sum = sum + prev;
-		if (sum <= 400000000)
+		if ((sum % 2) == 0)
 		{
-			if ((sum % 2) == 0)
-			{
-				sum_even += sum;
-			}
+			sum_even += sum;
 		}
 		prev = temp;
-		k++;
 	}
 	printf("%lu\n", sum_even);
 
