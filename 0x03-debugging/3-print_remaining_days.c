cfca102a5a7d43c7d78991819b758557e9385350
Getacher Demisse <getacher.d@gmail.com>
2022-03-16 18:07:38 +0300

    leap

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
     if ((year % 400 == 0) || ((year % 4 == 0) && !(year % 100 == 0)))
     {
-        if (month >= 2 && day >= 60)
+        if (month >= 2 && day <= 59)
         {
             day++;
         }
@@ -34,4 +34,9 @@ void print_remaining_days(int month, int day, int year)
             printf("Remaining days: %d\n", 365 - day);
         }
     }
+}
+int main(void)
+{
+	print_remaining_days(2,29,2000);
+	return 0;
 }
