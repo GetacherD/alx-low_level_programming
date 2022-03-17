fd11996970ea7fb188bd43ac43b4b46fedb7a5b6
Getacher Demisse <getacher.d@gmail.com>
2022-03-17 11:18:52 +0300

    update all for betty style

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,4 +4,8 @@ void _putchar(char s);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+int asci(int n);
+void more_numbers(void);
+void print_line(int n);
 #endif
