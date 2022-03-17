77eeff6a6d6ec53bdd4d56130eb3c7c2264e3cb6
GetacherD <getacher.d@gmail.com>
2022-03-17 09:02:21 -0700

    sign fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -42,6 +42,10 @@ long max_prime_factor(long n)
 	max = 1;
 	for (i = 1; i <= n; i++)
 	{
+		if (i > n / 2)
+		{
+			break;
+		}
 		if (n % i == 0)
 		{
 			/*printf("%lu\n", i);*/
@@ -52,7 +56,7 @@ long max_prime_factor(long n)
 					max = i;
 				}
 			}
-		}	
+		}
 	}
 
 	return (max);
