da6c88cd7741e69c6cb592d412388638f2133013
Getacher Demisse <getacher.d@gmail.com>
2022-03-21 13:02:06 +0300

    define func swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap two variables
+ * @a: first number
+ * @b: second number
+ *
+ * Description: swap two input values
+ */
+void swap_int(int *a, int *b)
+{
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
