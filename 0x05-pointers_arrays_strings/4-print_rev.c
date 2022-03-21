0ae138e8512a9329174e9bb18ccfe7542b20acc5
Getacher Demisse <getacher.d@gmail.com>
2022-03-21 14:40:13 +0300

    fix string declaration

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_rev -print string in reverse
+ * rev_string -print string in reverse
  * @s: input string
  *
  * Description: prints string in reverse order
  */
-void print_rev(char *s)
+void rev_string(char *s)
 {
 	int len;
 	int i;
-
+	
 	len = 0;
 	while (*s != '\0')
 	{
 		len++;
+		s++;
 	}
-	for (i = len - 2; i >= 0; i--)
+	for (i = 0; i <= len; i++)
 	{
-		_putchar(s[i]);
+		_putchar(*(s--));
+
 	}
 }
-
