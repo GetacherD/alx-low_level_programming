76125a10d55ceb1a700c0034d587b57eea420876
Getacher Demisse <getacher.d@gmail.com>
2022-03-22 16:24:43 +0300

    modify print array func

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,17 +11,21 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n >= 1)
+	if (n > 0)
 	{
-		for (i = 0; i < n - 1; i++)
+		for (i = 0; i < n; i++)
 		{
-			printf("%d, ", *(a + i));
+			if (i != n - 1)
+			{
+				printf("%d, ", a[i]);
+			}
+			else
+			{
+				printf("%d", a[i]);
+			}
 		}
-		printf("%d\n", *(a + n - 1));
 	}
-	else
-	{
-		printf("\n");
-	}
+	printf("\n");
+
 }
 
