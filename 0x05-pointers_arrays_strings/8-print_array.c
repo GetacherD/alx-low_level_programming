5a5a62d0d5264692ef440fb105c14343a7175c20
Getacher Demisse <getacher.d@gmail.com>
2022-03-22 03:24:02 +0300

    test

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_array -print array
- * @a: arry
- * @n: number 
+ * print_array -print arrya
+ * @a: array
+ * @n: num
  *
- * Description: print array
+ * Description:  print array
  */
 void print_array(int *a, int n)
 {
 	int i;
+
 	if (n >= 1)
 	{
 		for (i = 0; i < n - 1; i++)
