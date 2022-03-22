60d6c2e7d1ecca92e01a19a568a583aeffdfb811
Getacher Demisse <getacher.d@gmail.com>
2022-03-22 15:39:39 +0300

    fix out of range poniter

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,13 @@ void rev_string(char *s)
 	int length;
 	int i, j;
 	char tmp;
+	char *cp = s;
 
 	length = 0;
-	while (*s != '\0')
+	while (*cp != '\0')
 	{
 		length++;
-		s++;
+		cp++;
 	}
 	j = length - 1;
 	for (i = 0; i < length / 2; i++)
