9fe30e689fd84d9d01051f40fd9117b5d643c88d
Getacher Demisse <getacher.d@gmail.com>
2022-03-22 15:51:04 +0300

    fix typo

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,18 @@
  */
 void puts2(char *str)
 {
+	int len;
+	char *cp = s;
+
+	len = 0;
 	while (*str != '\0')
 	{
-		_putchar(*str);
+		len++;
 		str += 2;
+	}
+	for (i = 0; i < len - 1; i += 2)
+	{
+		_putchar(cp[i]);
 	}
 	_putchar('\n');
 }
