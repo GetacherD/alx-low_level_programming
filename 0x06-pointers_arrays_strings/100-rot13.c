316445e6334cf93b5e8b415cb6d747d33634c38e
Getacher Demisse <getacher.d@gmail.com>
2022-03-23 19:03:16 +0300

    rot13 function

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * rot -return rotated char
+ * @c: char tested
+ *
+ * Description: return rot13 encode char
+ *
+ * Return: new char
+ */
+int rot(char c)
+{
+	if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
+	{
+		c = c + 13;
+	}
+	else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
+	{
+		c = c - 13;
+	}
+
+	return (c);
+}
+
+/**
+ * rot13 -encode using rot13
+ * @s: string to be encoded
+ *
+ * Description: this function encode string  using rot13 method
+ *
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	char *cp = s;
+
+	while (*s != '\0')
+	{
+		*s = rot(*s);
+		s++;
+	}
+
+	return (cp);
+}
