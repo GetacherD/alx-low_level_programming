b87e411acbd1f7d3b27b11303e2c668d7e4cf5f7
GetacherDe <getacherd@ethiopianairlines.com>
2022-03-24 19:06:31 +0300

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -34,13 +34,28 @@ int rot(int x)
  */
 char *rot13(char *s)
 {
-	char *cp = s;
+	int i , j;
+	char alph[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V','W', 'X', 'Y', 'Z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char rot[52] ={'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V','W', 'X', 'Y', 'Z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+		'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
-	while (*s != '\0')
+	while (s[j] != '\0')
 	{
-		*s = rot(*s);
-		s++;
+		for (i = 0; i < 52 ; i++)
+		{
+			if (s [j] == alph[i])
+			{
+				s[j] = rot[i];
+				break;
+			}
+		}
+		j++;
 	}
 
-	return (cp);
+	return (s);
 }
