0014c7da14a90091907c2412621ab52c5984f330
GetacherD <getacher.d@gmail.com>
2022-03-25 17:18:29 -0700

    modify print number func

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,36 +1,74 @@
 #include "main.h"
-#include <limits.h>
 /**
- * prn -print in recurssion
- * @n: integer
+ * power - gives power of x  to y
+ * @x: base
+ * @y: exponent
  *
- * Description: print using recursion
+ * Description:  calculate x to the power of y
+ *
+ * Return: x ^ y
  */
-void prn(int n)
+
+int power(int x, int y)
 {
-	if ((n < 0  && n > INT_MIN )|| n > INT_MAX)
+	int i;
+	int cp;
+
+	cp = x;
+	for (i = 1; i < y; i++)
 	{
-		n = -n;
-		_putchar('-');
+		x *= cp;
 	}
 
-	if (n / 10 == 0)
+	return (x);
+}
+
+/**
+ * print_number -print any number
+ * @n: the number to be printed
+ *
+ * Description: this function prints any number
+ */
+
+void print_number(int n)
+{
+	int m;
+	int i;
+	int digits;
+	int b;
+	int sign;
+
+	sign = 1;
+	b = 10;
+	digits = 1;
+	m = n;
+	if (n < 0)
 	{
-		_putchar(n % 10 + '0');
+		_putchar('-');
+	}
+	while ((n / b) != 0)
+	{
+		digits++;
+		n = n / b;
+	}
+	for (i = digits - 1; i > 0; i--)
+	{
+		if (m < 0)
+		{
+			_putchar((-sign) * (m / power(b, i)) + '0');
+		}
+		else
+		{
+			_putchar(m / power(b, i) + '0');
+		}
+		m %= power(b, i);
+	}
+	if (m < 0)
+	{
+		_putchar((-sign) * m + '0');
 	}
 	else
 	{
-		prn(n / 10);
-		_putchar(n % 10 + '0');
+		_putchar(m + '0');
 	}
-}
-/**
- * print_number -print num
- * @n: integer to be printed
- *
- * Description: print using rec
- */
-void print_number(int  n)
-{
-	prn(n);
 }
