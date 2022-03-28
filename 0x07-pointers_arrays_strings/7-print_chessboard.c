c96073ee0fbaad79307c587d58cdc38b783807f6
GetacherD <getacher.d@gmail.com>
2022-03-28 08:35:41 -0700

    add _putchar function

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,12 +10,12 @@ void print_chessboard(char (*a)[8])
 	int i, j;
 
 	i = 0;
-	for (i = 0; i <8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
