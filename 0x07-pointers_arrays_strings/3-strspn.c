9456a60ffa328f3331d59e624729cf7619470ece
Getacher Demisse <getacher.d@gmail.com>
2022-03-28 12:59:22 +0300

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * _strspn -get length of prefix substring
+ * @s: string input
+ * @accept: char to find
+ *
+ * Description: get length of string initail segment
+ *
+ *  Return: length of initail segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int max;
+	unsigned int i;
+	unsigned int j;
+	unsigned int count;
+
+	max = 0;
+	i = 0;
+	j = 0;
+	count = 0;
+
+	while (accept[i] != '\0')
+	{
+		while (s[j] != '\0')
+		{
+			if (s[j] == accept[i])
+			{
+				break;
+			}
+			count++;
+			j++;
+		}
+		if (max <= count)
+		{
+			max = count;
+		}
+		count = 0;
+		i++;
+	}
+
+	return (max - 1);
+}
+
