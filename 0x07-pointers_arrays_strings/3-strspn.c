7d793e75ee58c4cd802f4078caefa5426dad38bd
Getacher Demisse <getacher.d@gmail.com>
2022-03-28 13:30:46 +0300

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,33 +13,30 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int max;
 	unsigned int i;
 	unsigned int j;
-	unsigned int count;
 
 	max = 0;
 	i = 0;
 	j = 0;
-	count = 0;
 
-	while (accept[i] != '\0')
+	while (s[i] != '\0')
 	{
-		while (s[j] != '\0')
+		while (accept[j] != '\0')
 		{
-			if (s[j] == accept[i])
+			if (s[i] == accept[j])
 			{
+				max++;
 				break;
 			}
-			count++;
+			else if (accept[j + 1] == '\0')
+			{
+				return (max);
+			}
 			j++;
 		}
-		if (max <= count)
-		{
-			max = count;
-		}
-		count = 0;
 		i++;
 		j = 0;
 	}
 
-	return (max + 1);
+	return (max);
 }
 
