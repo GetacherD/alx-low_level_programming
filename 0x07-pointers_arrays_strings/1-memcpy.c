5ffa939797b935edb6b05a09926ad4229db2f45b
Getacher Demisse <getacher.d@gmail.com>
2022-03-28 13:21:24 +0300

    use pointer copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,11 @@
  *
  * Return: pointer to destination
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	unsigned char *d = dest;
-	const unsigned char *s = src;
+	char *d = dest;
+	char *s = src;
 
 	for (i = 0; i < n; i++)
 	{
