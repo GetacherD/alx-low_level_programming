10a5ae85537db3e10f0a2728b0a2cdfc623ac876
Getacher Demisse <getacher.d@gmail.com>
2022-03-28 14:40:59 +0300

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * _strstr -locate substrig
+ * @haystack: main sytring
+ * @needle: substring
+ *
+ * Description: locate and return substring
+ *
+ * Return: pointer to substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *cp = haystack;
+	int found, i, j;
+
+	found = 1;
+	i = 0;
+	while (cp[j] != '\0')
+	{
+		while (needle[i] != '\0')
+		{
+			
+			if ((cp[j + i] == '\0') || (cp[j + i] != needle[i]))
+			{
+
+				found = found * -1;
+				break;
+
+			}
+			else
+			{
+				i++;
+			}
+	
+		}
+		if(found == 1)
+		{
+			return (&cp[j]);
+		}
+		found = 1;
+		i = 0;
+		j++;
+	}
+	if (found == -1)
+		return (0);
+	else
+		return (0);
+}
+
+
