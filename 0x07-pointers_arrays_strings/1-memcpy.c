113f300a865aa64722b1ee7f19f95197c81b7bf0
Getacher Demisse <getacher.d@gmail.com>
2022-03-29 03:15:18 +0300

    fix index

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,25 +1,22 @@
 #include "main.h"
-/**
- * _memcpy -copy memory data
- * @dest: destination memory block
- * @src: source memory block
- * @n: number of bytes to copy
- *
- * Description: copy memory block of fdata from src to dest
- *
- * Return: pointer to destination
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
-	char *d = dest;
-	char *s = src;
 
-	for (i = 0; i < n; i++)
-	{
-		d[i] = s[i];
-		i++;
-	}
+/**
+ * _memcpy - Copies @n bytes from the memory area pointed
+ *           to by @src into that pointed to by @dest.
+ * @dest: A pointer to the memory area to copy @src into.
+ * @src: The source buffer to copy characters from.
+ * @n: The number of bytes to copy from @src.
+ *
+ * Return: A pointer to the destination buffer @dest.
+ */
+void *_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned int index;
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
+
+	for (index = 0; index < n; index++)
+		destination[index] = source[index];
 
 	return (dest);
 }
