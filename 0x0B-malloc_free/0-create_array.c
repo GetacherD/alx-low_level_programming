5e2555ad68727159846d5a916ce0829da65c339d
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-04 16:17:38 +0300

    First Commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_array -create array
+ * @c: default value
+ * @size: size of array in bytes
+ *
+ * Description: create array and fill it with default
+ *
+ * Return: pointer to new array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	int i;
+
+	i =  0;
+	p = (char *)malloc(sizeof(char) * size);
+	if (p == NULL || size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		*(p + i) = c;
+	}
+
+	return (p);
+}
