8c0921a6237ad7389e04417e37c380cea697bed5
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-04 16:46:58 +0300

    replace null with empty str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,11 @@ char *str_concat(char *s1, char *s2)
 {
 	int i, j, len1, len2;
 	char *con;
-
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	i = 0;
 	j = 0;
 	while (s1[i] != '\0')
