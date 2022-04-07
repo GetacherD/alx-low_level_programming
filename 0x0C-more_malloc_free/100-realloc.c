b5ce85e1c6344a0e3e3f9e321f7bfac673c22652
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-07 22:18:36 +0300

    return null when new size is null

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,8 +26,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size == 0 && ptr == NULL)
 		return (NULL);
-	if (new_ptr == NULL)
-		return (NULL);
 	if (ptr == NULL)
 	{
 		new_ptr = malloc(new_size);
