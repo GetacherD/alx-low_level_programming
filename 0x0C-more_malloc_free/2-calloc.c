5c431c81c00ea8e03d12c16e09b66165633ecb69
GetacherDe <getacherd@ethiopianairlines.com>
2022-04-07 12:57:22 +0300

    fill void pointer

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (mem == NULL)
 		return (NULL);
 	filler = mem;
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		filler[i] = '\0';
 
 	return (mem);
