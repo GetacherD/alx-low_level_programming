07f0b83791ee583f1e5f1dde92821e003d81fbbb
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-08 15:41:21 +0300

    fix betty style

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,8 +13,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
-	char *filler;
-	char *ptc;
+	char *filler, *ptc;
 	unsigned int i, j;
 
 	if (new_size == old_size)
@@ -27,8 +26,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr == NULL)
 	{
 		new_ptr = malloc(old_size);
-		if (new_ptr == NULL)
-			return (0);
 		return (new_ptr);
 	}
 	if (ptr == NULL)
