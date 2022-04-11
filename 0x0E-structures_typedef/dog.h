67a343df46cc101d703271833602dc0a7005a169
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-11 12:18:46 +0300

    modify comments

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,18 +8,26 @@
  *
  * Description: dog object having 3 props
  */
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-typedef struct 
+/**
+ * struct dog -define new object
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: define new data type
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
