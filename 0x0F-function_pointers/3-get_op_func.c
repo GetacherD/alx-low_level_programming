01617c8eb757b50916600864679b2f65fe88deda
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-13 14:29:43 +0300

    first commit

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+/**
+ * get_op_func -retunn which operation to be called
+ * @s: selector char input
+ *
+ * Description: operation selector funct
+ *
+ * Return: index of call back function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+	if (s == NULL)
+		return (NULL);
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (*ops[i].op == s[0])
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
