f5fb88045755910bc1e46460ad238edef1733db3
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-13 12:33:32 +0300

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name -prints name decorated
+ * @name: name given
+ * @f: decorator function ponter
+ *
+ * Description: print name diffrently based on function input
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
