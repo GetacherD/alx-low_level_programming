49bba1a02393007aa8e5fb491dfdfc8d75eefbeb
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-13 19:24:19 +0300

    ops code

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * @argc: number of argumnets
+ * @argv: argument vector
+ * Description: main program
+ *
+ * Return: 0 always
+ */
+int main(int __attribute__((unused)) argc, char **argv)
+{
+	FILE *fp;
+
+	char ch;
+	int i;
+
+	fp = fopen(__FILE__, "r");
+	if (fp == NULL)
+		return (-1);
+	for (i = 0; i < atoi(argv[1]); i++)
+	{
+		ch = fgetc(fp);
+		if (i > 108)
+		{
+			printf("%x\t", ch);
+		}
+	}
+	fclose(fp);
+	printf("\n");
+	return (0);
+}
