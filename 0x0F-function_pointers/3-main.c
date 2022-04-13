dc3d7a0ee7ae6f6ba3fa6db0ac43720171a3486d
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-13 18:05:43 +0300

    remove unused include

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,3 @@
-#include "function_pointers.h"
 #include <stdio.h>
 #include "3-calc.h"
 #include <stdlib.h>
@@ -23,12 +22,12 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[argc - 1]) == 0))
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[argc - 1])));
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
