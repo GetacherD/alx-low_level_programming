ca2d8ae585893af16779cbe127d08b96e7680838
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-13 16:33:50 +0300

    test

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,17 +16,17 @@ int main(int argc, char **argv)
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return(98);
 	}
 	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
-		exit(99);
+		return(99);
 	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[argc - 1]) == 0))
 	{
 		printf("Error\n");
-		exit(100);
+		return(100);
 	}
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[argc - 1])));
 
