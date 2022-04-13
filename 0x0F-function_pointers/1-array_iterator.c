350ff875c87a8342cf95a1254adb92e2cf56f428
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-13 13:06:58 +0300

    fix typo

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,18 +1,17 @@
 #include "function_pointers.h"
-/*
+/**
  * array_iterator -function that takes function
  * @array: function pointer array
- * @size_t: size of array
+ * @size: size of array
  * @action: function call back function
  *
  * Description: function that takes a call back func
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
-	
-	for (i = 0; i < size / sizeof(array[0]); i++)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
