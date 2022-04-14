9a7fff67c6f0748b9caad4bf6b3ea29809eee9b9
GetacherD <getacher.d@gmail.com>
2022-04-14 02:08:55 -0700

    modify sepa condition

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,17 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, int);
-		if (separator == NULL)
-		{
-			printf("%d", x);
-		}
-		else
-		{
-			printf("%d", x);
-			if (i == n - 1)
-				continue;
-			printf("%s", separator);
-		}
+		printf("%d", x);
+		if (separator == NULL || i == n - 1)
+			continue;
+		printf("%s", separator);
 	}
 	va_end(args);
 	printf("\n");
