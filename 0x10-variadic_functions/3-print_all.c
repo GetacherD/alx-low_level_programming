efc3081496426b9f145fd16f108955874e3ecf55
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-14 20:27:20 +0300

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,37 +1,4 @@
 #include "variadic_functions.h"
-void print_c(va_list list);
-void print_i(va_list list);
-void print_f(va_list list);
-void print_s(va_list list);
-void print_a(va_list list);
-/**
- * print_all -print every thing
- * @format: format string
- *
- * Description: print string based on format
- */
-void print_all(const char *const format, ...)
-{
-	char fmt[] = {'c', 'i', 'f', 's', 'a'};
-	unsigned int i, j;
-	va_list args;
-	void (*ptr_print[])(va_list) = {print_c, print_i, print_f, print_s, print_a};
-	va_start(args, format);
-	i = j = 0;
-	if (format == NULL)
-		return;
-	while (format[i] != '\0')
-	{
-		while (format[i] != fmt[j] && j < 4)
-			j++;
-		ptr_print[j](args);
-		if (j != 4 && format[i + 1] != '\0')
-			printf(", ");
-		j = 0;
-		i++;
-	}
-	printf("\n");
-}
 /**
  * print_s -print string
  * @list: arg list
@@ -78,11 +45,31 @@ void print_c(va_list list)
 	printf("%c", va_arg(list, int));
 }
 /**
- * print_a -print nothing
- * @list: arg list
+ * print_all -print every thing
+ * @format: format string
  *
- * Description: fail callback
+ * Description: print string based on format
  */
-void print_a(va_list __attribute__((unused)) list)
+void print_all(const char *const format, ...)
 {
+	char fmt[] = {'c', 'i', 'f', 's'};
+	int i, j;
+	va_list args;
+	void (*ptr_print[])(va_list) = {print_c, print_i, print_f, print_s};
+
+	va_start(args, format);
+	i = j = 0;
+	while (format[i] != '\0')
+	{
+		while ((format[i] != fmt[j]))
+			j++;
+		if (j < 4)
+		{
+			ptr_print[j](args);
+			printf(", ");
+		}
+		j = 0;
+		i++;
+	}
+	printf("\n");
 }
