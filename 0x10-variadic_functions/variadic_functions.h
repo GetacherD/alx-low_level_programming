8116dc5119f927e1b723666c1558b8a971778d09
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-14 21:06:23 +0300

    test

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,14 +1,7 @@
 #ifndef VARF
 #define VARF
-#include <stdarg.h>
-#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
-void print_c(va_list list);
-void print_i(va_list list);
-void print_f(va_list list);
-void print_s(va_list list);
-void print_a(va_list list);
 #endif
