6cbcef5e13d382fbba7671415171c04bbee185bb
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-21 17:22:12 +0300

    add to end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *Node, *p;
 	unsigned int i;
 
-	Node =malloc(sizeof(list_t));
+	Node = malloc(sizeof(list_t));
 	if (Node == NULL)
 		return (*head);
 	Node->str = strdup(str);
