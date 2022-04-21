d7960506a5336a581236970313fd5d173120bac3
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-21 17:55:54 +0300

    function before

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,6 +4,6 @@
  */
 void fun_before(void)
 {
-	printf("%s", "You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("%s", "You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
