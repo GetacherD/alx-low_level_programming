83b6020ef60aa44eef4b7d16f50889b419ee8349
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-22 23:19:46 +0300

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,20 @@
-global main
 extern printf
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
 
 section .data
-	msg db "Hello, Holberton", 0x0A, 0x00
-	len equ $ -msg
-main:
-	push rbp
-	mov rbp, rsp
-	push len
-	push msg
-	call printf
-	mov rax , 0
-	mov rsp ,rbp
-	pop rbp
-	ret
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
