20efbe6433b372f73d966f0a86634e4bce1ebc7c
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-25 19:41:32 +0300

    fix index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	i = 0;
 	while (head && (i < index))
 	{
-		if(head == NULL)
+		if (head == NULL)
 			return (NULL);
 		i++;
 		head = head->next;
