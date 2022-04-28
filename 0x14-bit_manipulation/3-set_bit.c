45c4c724ff85e5a3bd5e6c63762983d7c358601f
GetacherD <GETACHER.D@GMAIL.COM>
2022-04-28 12:51:35 +0300

    fix for index > 32

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,8 +8,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int long or = 0x01 << index;
+	unsigned int long or = 1;
+	or = or << index;
 	if (n == NULL)
+		return (-1);
+	if (index > 32)
 		return (-1);
 	*n = (*n) | or;
 	return (1);
