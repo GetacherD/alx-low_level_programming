a1cd00c35d58a509ac72bc1ded6c77b80d744159
Getacher <Getacher.d@gmail.com>
2022-05-02 17:17:47 +0300

    reduce extra lines

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,31 @@
 #include "main.h"
+/**
+* closeFile - close file by file desc
+* @fd: file descriptor
+*/
+void closeFile(int fd)
+{
+	int c;
 
+	c = close(fd);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+* check_args -check args count
+* @argc: arg count
+*/
+void check_args(int argc)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
 /**
 * main - entry point to program
 * @argc: argumnet count
@@ -8,61 +34,44 @@
 */
 int main(int argc, char **argv)
 {
-	ssize_t r, w, len, fd, fdr;
-	off_t end, cur;
+	ssize_t r, end, cur, w, len = 0;
+	int fd, fdr;
 	char *buf;
 
-	len = 0;
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fd = (int)open(argv[1], O_RDONLY);
+	check_args(argc);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	cur = lseek((int)fd, 0, SEEK_CUR);
-	end = lseek((int)fd, 0, SEEK_END);
-	len = (end - cur);
+	cur = (off_t)lseek(fd, 0, SEEK_CUR);
+	len = lseek(fd, 0, SEEK_END) - (ssize_t)cur;
+	end = (off_t)lseek(fd, 0, SEEK_CUR);
 	buf = malloc(sizeof(char) * 1024);
-	lseek((int)fd, -len, SEEK_CUR);
+	lseek(fd, -len, SEEK_CUR);
 	if (buf == NULL)
 		return (-1);
 	fdr = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	while (lseek((int)fd, 0, SEEK_CUR) != end)
+	while (lseek(fd, 0, SEEK_CUR) != (ssize_t)end)
 	{
-
-		r = read((int)fd, buf, 1024);
+		r = read(fd, buf, 1024);
 		if (r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			free(buf);
 			exit(98);
 		}
-		w = (int)write((int)fdr, buf, 1024);
-		if (w == -1 || (int)fdr == -1)
+		w = write(fdr, buf, 1024);
+		if (w == -1 || fdr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			free(buf);
 			exit(99);
 		}
 	}
-	if (close((int)fdr) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (int)fdr);
-		free(buf);
-		exit(100);
-	}
-	if (close((int)fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (int)fd);
-		free(buf);
-		exit(100);
-	}
-
 	free(buf);
+	closeFile(fd);
+	closeFile(fdr);
 	return (1);
 }
