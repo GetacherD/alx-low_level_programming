eff79868ca4abef5bb4ed4a8a7681526a0fd8ea9
Getacher <Getacher.d@gmail.com>
2022-06-09 13:51:16 +0300

    fix alloc

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,25 +11,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *newNode, *cur;
 	unsigned int i = 0;
 
+	if (idx == 0 || !(*h))
+		return (add_dnodeint(h, n));
+	cur = *h;
+	for (i = 0; (i < idx) && cur; i++)
+		cur = cur->next;
+	if (cur == NULL)
+		return (add_dnodeint_end(h, n));
 	newNode = malloc(sizeof(dlistint_t));
 	if (!newNode)
 		return (NULL);
 	newNode->n = n;
 	newNode->next = NULL;
 	newNode->prev = NULL;
-	if (idx == 0 || !(*h))
-	{
-		free(newNode);
-		return (add_dnodeint(h, n));
-	}
-	cur = *h;
-	for (i = 0; (i < idx ) && cur; i++)
-		cur = cur->next;
-	if (cur == NULL)
-	{
-		free(newNode);
-		return (add_dnodeint_end(h, n));
-	}
+
 	(cur->prev)->next = newNode;
 	newNode->next = cur;
 	newNode->prev = cur->prev;
