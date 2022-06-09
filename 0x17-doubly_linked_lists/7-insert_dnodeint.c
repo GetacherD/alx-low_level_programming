c1fff6757eef203c048154dd4cd50004240c4f46
Getacher <Getacher.d@gmail.com>
2022-06-09 14:03:45 +0300

    fix i

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		cur = cur->next;
 		i++;
 	}
-	if (i != (idx - 1))
+	if (i != idx)
 		return (NULL);
 	if (cur == NULL)
 		return (add_dnodeint_end(h, n));
