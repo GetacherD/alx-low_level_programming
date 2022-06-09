5483bbfe8c3f1fc519969cf275d5bb4a8d4ccbbb
Getacher <Getacher.d@gmail.com>
2022-06-09 12:51:12 +0300

    test

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,11 +13,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	cur = head;
-	for (i = 0; i <= index; i++)
+	for (i = 0; (i < index) && cur; i++)
 	{
-		if (cur == NULL)
-			return (NULL);
 		cur = cur->next;
 	}
-	return (cur->prev);
+	return (cur);
 }
