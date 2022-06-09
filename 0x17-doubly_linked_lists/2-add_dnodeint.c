303d70f723c0f17eceaaed204c6cdf8b722f5a1b
Getacher <Getacher.d@gmail.com>
2022-06-09 12:45:50 +0300

    fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!newNode)
 		return (NULL);
 	newNode->n = n;
-	newNode->next = NULL;
 	newNode->prev = NULL;
 	newNode->next = *head;
 	if (*head)
