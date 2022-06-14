0e8841ddb9652f0cc18073e625f1cb56ff421821
Getacher <Getacher.d@gmail.com>
2022-06-14 13:23:03 +0300

    test

--- 0x18-dynamic_libraries/fake.c
+++ 0x18-dynamic_libraries/fake.c
@@ -1,15 +1,15 @@
 #include <unistd.h>
-int puts(const char *s)
+int puts(const char __attribute__((unused))*s)
 {
 	write(1, "Congratulations, you win the Jackpot!\n", 38);
 }
-int printf(const char *format, ...)
+int printf(const char __attribute__((unused))*format, ...)
 {
-	static int x = 1;
-	if (x == 1)
+	static int x = 0;
+	if (!x)
 	{
 		write(1, "9 8 10 24 75 9\n", 16);
-		x = 2;
+		x = 1;
 	}
 	return (0);
 }
