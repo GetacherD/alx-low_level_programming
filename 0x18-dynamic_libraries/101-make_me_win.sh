f8e4573dd8bba30a1fc323bd733f10ae898b9cea
Getacher <Getacher.d@gmail.com>
2022-06-14 13:03:03 +0300

    fix dir

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/GetacherD/alx-low_level_programming/blob/main/0x18-dynamic_libraries/fake.so 
-export LD_PRELOAD=./fake.so
+wget https://github.com/GetacherD/alx-low_level_programming/blob/main/0x18-dynamic_libraries/fake.so -P ../
+export LD_PRELOAD=../fake.so
