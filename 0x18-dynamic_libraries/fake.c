9a7845d1d327c7a3ae1743711e6d09b61c6482ac
GetacherD <getacher.d@gmail.com>
2022-06-17 15:35:52 -0700

    test

--- 0x18-dynamic_libraries/fake.c
+++ 0x18-dynamic_libraries/fake.c
@@ -1,15 +1,37 @@
+#include <stdio.h>
 #include <unistd.h>
-int puts(const char __attribute__((unused))*s)
-{
-	write(1, "Congratulations, you win the Jackpot!\n", 38);
-}
-int printf(const char __attribute__((unused))*format, ...)
-{
-	static int x = 0;
-	if (!x)
+int dprintf(int fd, const char *format, ...){
+
+/*	static int i = 0;
+	if (i == 0)
 	{
-		write(1, "9 8 10 24 75 9\n", 16);
-		x = 1;
+		i++;
+		return (write(1, "9 8 10 24 75 9\n",15));
 	}
+*/
+return (0);
+}
+
+int printf(const char *format, ...){
+
+	static int i = 0;
+	if (i == 0)
+	{
+		i++;
+		/*return (write(1, "9 8 10 24 75 9\n",15));*/
+		return (write(1, "Congratulations, you win the Jackpot!\n",38));
+	}
+
+
+return (0);	
+}
+int puts(const char *s)
+{
+/*	static int i = 0;
+	if (i == 0)
+	{
+		i++;
+		return (write(1, "Congratulations, you win the Jackpot!\n",38));
+	}*/
 	return (0);
 }
