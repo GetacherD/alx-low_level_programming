df38627ca68bfb908b80f9a2f7c14a86c9b1b8fe
GetacherD <getacher.d@gmail.com>
2022-06-30 13:47:37 -0700

    fix typo

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -95,7 +95,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	while (cur && _strcmp(cur->key, key)  != 1)
 		cur = cur->next;
 	if (cur == NULL)
-		add_key_collision(ht, key, value);
+		return (add_key_collision(ht, key, value));
 	free(cur->value);
 	cur->value = (char *)malloc(sizeof(char) * _strlen(value));
 	if (cur->value == NULL)
