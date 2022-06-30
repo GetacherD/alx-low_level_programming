14f5e324ff97961c73125cb7846baac597f00c0a
GetacherD <getacher.d@gmail.com>
2022-06-30 14:01:35 -0700

    first

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,32 +1,31 @@
 #include "hash_tables.h"
+/**
+ * hash_table_print - print hash table data
+ * @ht: hash table ptr
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *Node;
 	unsigned long int i = 0;
 	unsigned long int is_first = 1;
-	if (ht)
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
 	{
-		printf("{");
-		for (i = 0; i < ht->size; i++)
+		if ((ht->array)[i])
 		{
-			if ((ht->array)[i])
-			 {
-			   
-				 Node = (ht->array)[i];
-				 while(Node)
-				 {
-					 if (!is_first)
-						 printf(",");
-					 printf("'%s': '%s'", Node->key, Node->value);
-					 is_first = 0;
-					 Node = Node->next;
-					 if (Node)
-						 printf(" ====printing ever collison====");
-				 }
-				 
-			 }
+			Node = (ht->array)[i];
+			while (Node)
+			{
+				if (!is_first)
+					printf(",");
+				printf("'%s': '%s'", Node->key, Node->value);
+				is_first = 0;
+				Node = Node->next;
+			}
 		}
-		printf("}\n");
 	}
-			
+	printf("}\n");
 }
