75d13fbc9aa338a3688dca620189376b37d35975
GetacherD <getacher.d@gmail.com>
2022-06-30 14:28:41 -0700

    test

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,6 +8,8 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *Node, *cur;
 
+	if (ht == NULL)
+		return;
 	for (i = 0; i < ht->size; i++)
 	{
 		cur = (ht->array)[i];
