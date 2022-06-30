99b3777b0e672fd97b521d13a9fa25f199099676
GetacherD <getacher.d@gmail.com>
2022-06-30 09:32:55 -0700

    fix style

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,4 +1,10 @@
 #include "hash_tables.h"
+/**
+ * key_index - get hashed index
+ * @key:key to be hashed
+ * @size: size of hash table
+ * Return: index of key in the table
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
