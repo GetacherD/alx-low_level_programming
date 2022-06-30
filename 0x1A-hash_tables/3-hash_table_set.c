d0d62e650bdc0d4304f911f5cea002380077a78d
GetacherD <getacher.d@gmail.com>
2022-06-30 10:03:25 -0700

    test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -50,6 +50,34 @@ int _strlen(const char *s)
 		i++;
 	return (i + 1);
 }
+int add_key(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i;
+	hash_node_t *Node;
+
+	i = key_index((const unsigned char *)key, ht->size);
+	Node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (Node == NULL)
+		return (0);
+	Node->key = (char *)malloc(sizeof(char) * _strlen(key));
+	if (Node->key == NULL)
+	{
+		free(Node);
+		return (0);
+	}
+	Node->value = (char *)malloc(sizeof(char) * _strlen(value));
+	if (Node->value == NULL)
+	{
+		free(Node->key);
+		free(Node);
+		return (0);
+	}
+	Node->next = NULL;
+	_strcpy(key, Node->key);
+	_strcpy(value, Node->value);
+	(ht->array)[i] = Node;
+	return (1);
+}
 /**
  * hash_table_set - add new element to table
  * @ht: hash table
@@ -71,6 +99,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (Node == NULL)
 		return (0);
 	if ((ht->array)[i] == NULL)
+		return (add_key(ht, key, value));
+	cur = (ht->array)[i];
+	while (cur && _strcmp(cur->key, key)  != 1)
+		cur = cur->next;
+	if (cur == NULL)
 	{
 		Node->key = (char *)malloc(sizeof(char) * _strlen(key));
 		if (Node->key == NULL)
@@ -85,41 +118,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(Node);
 			return (0);
 		}
-		Node->next = NULL;
 		_strcpy(key, Node->key);
 		_strcpy(value, Node->value);
+		Node->next = (ht->array)[i];
 		(ht->array)[i] = Node;
 	}
 	else
 	{
-		cur = (ht->array)[i];
-		while (cur && _strcmp(cur->key, key)  != 1)
-			cur = cur->next;
-		if (cur == NULL)
-		{
-			Node->key = (char *)malloc(sizeof(char) * _strlen(key));
-			if (Node->key == NULL)
-			{
-				free(Node);
-				return (0);
-			}
-			Node->value = (char *)malloc(sizeof(char) * _strlen(value));
-			if (Node->value == NULL)
-			{
-				free(Node->key);
-				free(Node);
-				return (0);
-			}
-			_strcpy(key, Node->key);
-			_strcpy(value, Node->value);
-			Node->next = (ht->array)[i];
-			(ht->array)[i] = Node;
-		}
-		else
-		{
-			free(cur->value);
-			cur->value = (char *)malloc(sizeof(char) * _strlen(value));
-			if (cur->value == NULL)
+		free(cur->value);
+		cur->value = (char *)malloc(sizeof(char) * _strlen(value));
+	{
+		if (cur->value == NULL)
 			{
 				free(Node->key);
 				free(Node);
