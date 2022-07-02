1a8eb01828ae8fe20b030fd9feabd6834d37418f
Getacher <Getacher.d@gmail.com>
2022-07-02 13:31:56 +0300

    refactor to reduce line

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -50,6 +50,92 @@ int __lt__(const char *Node, const char *cur)
 	return (-1);
 }
 /**
+ * _add_shash_table - add new element to hash table
+ * @ht: hash table sorted
+ * @Node: node to be added
+ * Return: 1 if succed else 0
+ */
+int  _add_shash_table(shash_table_t *ht, shash_node_t *Node)
+{
+	shash_node_t *cur, *prev;
+
+	if (ht->shead == NULL)
+	{
+		ht->shead = Node;
+		ht->stail = Node;
+		Node->snext = NULL;
+		Node->sprev = NULL;
+		return (1);
+	}
+	cur = ht->shead;
+	prev = ht->shead;
+	while (cur)
+	{
+		if (__lt__(Node->key, cur->key) == 1)
+		{
+			if (cur == ht->shead)
+			{
+				cur->sprev = Node;
+				Node->snext = cur;
+				Node->sprev = NULL;
+				ht->shead = Node;
+			}
+			else
+			{
+				Node->snext = cur;
+				Node->sprev = prev;
+				cur->sprev = Node;
+				prev->snext = Node;
+			}
+			return (1);
+		}
+		prev = cur;
+		cur = cur->snext;
+	}
+	(ht->stail)->snext = Node;
+	Node->snext = NULL;
+	Node->sprev = ht->stail;
+	ht->stail = Node;
+	return (1);
+}
+/**
+ * add_new - add new node to hash table
+ * @ht: hash table
+ * @key: new key
+ * @value: new value
+ * Return:  1 if succeed else 0
+ */
+int add_new(shash_table_t *ht, const char *key, const char *value)
+{
+	shash_node_t *Node;
+	unsigned long int i;
+
+	i = key_index((const unsigned char *)key, ht->size);
+	Node = (shash_node_t *)malloc(sizeof(shash_node_t));
+	if (Node == NULL)
+		return (0);
+	Node->key = malloc(sizeof(char) * _strlen(key));
+	if (Node->key == NULL)
+	{
+		free(Node);
+		return (0);
+	}
+	Node->value = malloc(sizeof(char) * _strlen(value));
+	if (Node->value == NULL)
+	{
+		free(Node->key);
+		free(Node);
+		return (0);
+	}
+
+	_strcpy(key, Node->key);
+	_strcpy(value, Node->value);
+	Node->next = (ht->array)[i];
+	(ht->array)[i] = Node;
+	return (_add_shash_table(ht, Node));
+}
+
+/**
  * shash_table_set - set element to ht
  * @ht: hash table
  * @key: new key
@@ -65,152 +151,22 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 		return (0);
 	i = key_index((const unsigned char *)key, ht->size);
 	if ((ht->array)[i] == NULL)
+		return (add_new(ht, key, value));
+	cur = (ht->array)[i];
+	while (cur && _strcmp(cur->key, key) != 1)
+		cur = cur->next;
+	if (cur == NULL)
+		return (add_new(ht, key, value));
+	free(cur->value);
+	cur->value = malloc(sizeof(char) * _strlen(value));
+	if (cur->value == NULL)
 	{
-		Node = (shash_node_t *)malloc(sizeof(shash_node_t));
-		if (Node == NULL)
-			return (0);
-		Node->key = malloc(sizeof(char) * _strlen(key));
-		if (Node->key == NULL)
-		{
-			free(Node);
-			return (0);
-		}
-		Node->value = malloc(sizeof(char) * _strlen(value));
-		if (Node->value == NULL)
-		{
-			free(Node->key);
-			free(Node);
-			return (0);
-		}
-
-		_strcpy(key, Node->key);
-		_strcpy(value, Node->value);
-		Node->next = (ht->array)[i];
-		(ht->array)[i] = Node;
-		if (ht->shead == NULL)
-		{
-			ht->shead = Node;
-			ht->stail = Node;
-			Node->snext = NULL;
-			Node->sprev = NULL;
-			return (1);
-		}
-		cur = ht->shead;
-		prev = ht->shead;
-		while (cur)
-		{
-			if (__lt__(Node->key, cur->key) == 1)
-			{
-				if (cur == ht->shead)
-				{
-					cur->sprev = Node;
-					Node->snext = cur;
-					Node->sprev = NULL;
-					ht->shead = Node;
-				}
-				else
-				{
-					Node->snext = cur;
-					Node->sprev = prev;
-					cur->sprev = Node;
-					prev->snext = Node;
-				}
-				return (1);
-			}
-			prev = cur;
-			cur = cur->snext;
-		}
-		(ht->stail)->snext = Node;
-		Node->snext = NULL;
-		Node->sprev = ht->stail;
-		ht->stail = Node;
-		return (1);
+		free(cur->key);
+		free(cur);
+		return (0);
 	}
-	else
-	{
-		cur = (ht->array)[i];
-		while (cur && _strcmp(cur->key, key) != 1)
-			cur = cur->next;
-		if (cur == NULL)
-		{
-			Node = (shash_node_t *)malloc(sizeof(shash_node_t));
-			if (Node == NULL)
-				return (0);
-			Node->key = malloc(sizeof(char) * _strlen(key));
-			if (Node->key == NULL)
-			{
-				free(Node);
-				return (0);
-			}
-			Node->value = malloc(sizeof(char) * _strlen(value));
-			if (Node->value == NULL)
-			{
-				free(Node->key);
-				free(Node);
-				return (0);
-			}
-			_strcpy(key, Node->key);
-			_strcpy(value, Node->value);
-			Node->next = (ht->array)[i];
-			(ht->array)[i] = Node;
-			if (ht->shead == NULL)
-			{
-				Node->sprev = NULL;
-				Node->snext = NULL;
-				ht->shead = Node;
-				ht->stail = Node;
-				return (1);
-			}
-			cur = ht->shead;
-			prev = ht->shead;
-			while (cur)
-			{
-				if (__lt__(Node->key, cur->key) == 1)
-				{
-					if (cur == ht->shead)
-					{
-						Node->snext = cur;
-						Node->sprev = NULL;
-						cur->sprev = Node;
-						ht->shead = Node;
-					}
-					else
-					{
-						Node->snext = cur;
-						Node->sprev = prev;
-						cur->sprev = Node;
-						prev->snext = Node;
-					}
-					return (1);
-				}
-				prev = cur;
-				cur = cur->snext;
-			}
-			(ht->stail)->snext = Node;
-			Node->snext = NULL;
-			Node->sprev = ht->stail;
-			ht->stail = Node;
-			return (1);
-		}
-		else
-		{
-			free(cur->value);
-			cur->value = malloc(sizeof(char) * _strlen(value));
-			if (cur->value == NULL)
-			{
-				free(cur->key);
-				free(cur);
-				return (0);
-			}
-			_strcpy(value, cur->value);
-			return (1);
-
-		}
-
-
-
-	}
-
+	_strcpy(value, cur->value);
+	return (1);
 }
 /**
  * shash_table_get - get element
