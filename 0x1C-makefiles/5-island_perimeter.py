8a20f3f113cbb22fb863f07b21de86fb7f5e1c3d
GetacherD <getacher.d@gmail.com>
2022-08-17 21:55:25 +0300

    fix start

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,14 +8,14 @@ def island_perimeter(grid):
 
     """ Calculate Perimeter of island """
     count = 0
-    if not grid or (grid and (len(grid) < 2 or len(grid[0]) < 2)):
+    if not grid or grid == [[]]:
         return 0
-    for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[0]) - 1):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
             if grid[i][j]:
-                if grid[i][j-1]:
+                if j > 0 and grid[i][j-1]:
                     count -= 2
-                if grid[i-1][j]:
+                if i > 0 and grid[i-1][j]:
                     count -= 2
                 count += 4
     return count
