3e9df15e9240244736d0f1e3f17b16c18c611404
GetacherD <getacher.d@gmail.com>
2022-10-05 08:47:31 +0300

    first

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,49 @@
+#include "search_algos.h"
+/**
+ * _binary_search - search sorted array for a value
+ * @array: array to be searched
+ * @size: size of array
+ * @value: value to be searched
+ * @low: start index
+ * @high: end index
+ * Return: index of value if found else -1
+ */
+int _advanced_binary(int *array, size_t size,
+		int value, size_t low, size_t high)
+{
+	size_t i = 0;
+	size_t mid;
+
+	if (array == NULL)
+		return (-1);
+	printf("Searching in array: %d", array[low]);
+	for (i = low + 1; i <= high; i++)
+	{
+		printf(", %d", array[i]);
+	}
+	printf("\n");
+	mid = low + (high - low) / 2;
+	if (array[mid] == value)
+		return (mid);
+	if (low == high)
+		return (-1);
+	if (array[mid] < value)
+		return (_advanced_binary(array, high - mid, value, mid + 1, high));
+	return (_advanced_binary(array, mid, value, low, mid - 1));
+}
+/**
+ * advanced_binary - search sorted array for a value
+ * @array: array to be searched
+ * @size: size of array
+ * @value: value to be searched
+ * Return: index of value if found else -1
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	size_t mid;
+
+	if (array == NULL)
+		return (-1);
+	return (_advanced_binary(array, size, value, 0, size - 1));
+}
